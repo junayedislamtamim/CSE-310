a24369c8e29f7dfb8693b9e297e05624a2e3d338
junayedislamtamim <junayedislamtamim8@gmail.com>
2026-07-02 14:56:33 +0600

    Add input function and output formatter in main
    Fix input and output formatting inside main, output formatting inside
    SymbolInfo and ScopeTable still needs to be fixed

---
+++ main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "SymbolTable.h"
+
+using namespace std;
+
+string formatSymbolType(string rest)
+{
+    stringstream ss(rest);
+    string token, symbolType;
+    ss >> token;
+
+    symbolType.reserve(250);
+    /*
+        GENERATES A FORMAT LIKE THIS:
+        FUNCTION, RETURN_TYPE <==(argument1_type, argument2_type, ..., arguementN_type)
+        FROM THIS:
+        FUNCTION INT INT FLOAT INT
+    */
+    if (token == "FUNCTION")
+    {
+        symbolType += ", ";
+        ss >> token;
+        symbolType += token + " <==(";
+
+        if(ss >> token)
+            symbolType += token;
+        
+        while (ss >> token)
+        {
+            symbolType += ", " + token;
+        }
+
+        symbolType += ")";
+    }
+    /*
+        GENERATES A FORMAT LIKE THIS:
+        STRUCT, {(INT, a), (FLOAT, b)}
+        FROM THIS:
+        foo STRUCT INT a FLOAT b
+    */
+    else if (token == "STRUCT" || token == "UNION")
+    {
+        symbolType += ", ";
+        symbolType += "{";
+
+        if(ss >> token)
+        {
+            symbolType += "(" + token + ", ";
+            ss >> token;
+            symbolType += token + ")";
+        }
+
+        while(ss >> token)
+        {
+            symbolType += ", (" + token + ", ";
+            ss >> token;
+            symbolType += token + ")";
+        }
+
+        symbolType += "}";
+    }
+
+    return symbolType;
+}
+
+int main(int argc, char **argv)
+{
+    // if(argc != 3)
+    // {
+    //     cerr<<"Wrong command line arguements\n"<<"Enter in the following format:\n<program> <inputfile> <outputfile>\n";
+    //     return 1;
+    // }
+
+    // freopen(argv[1], "r", stdin);
+    // freopen(argv[2], "w", stdout);
+    int n;
+    cin >> n;
+
+    SymbolTable *symbolTable = new SymbolTable();
+    string inputLine, token;
+    bool flag = true;
+
+    while (getline(cin, inputLine) && flag)
+    {
+        stringstream ss = stringstream(inputLine);
+        while (ss >> token)
+        {
+            string symbolName, symbolType;
+            if (token == "I")
+            {
+                ss >> symbolName;
+                getline(ss, inputLine);
+                symbolType = formatSymbolType(inputLine);
+                symbolTable->insertSymbol(symbolName, symbolType);
+            }
+            else if (token == "L")
+            {
+                ss >> symbolName;
+                symbolTable->lookUp(symbolName);
+            }
+            else if (token == "D")
+            {
+                ss >> symbolName;
+                symbolTable->deleteSymbol(symbolName);
+            }
+            else if (token == "P")
+            {
+                ss >> token;
+                if (token == "C")
+                    symbolTable->printCurrentScopeTable();
+                else if (token == "A")
+                    symbolTable->printAllScopeTables();
+            }
+            else if (token == "S")
+            {
+                symbolTable->enterScope();
+            }
+            else if (token == "E")
+            {
+                symbolTable->exitScope();
+            }
+            else if (token == "Q")
+            {
+                flag = false;
+                break;
+            }
+        }
+    }
+}
